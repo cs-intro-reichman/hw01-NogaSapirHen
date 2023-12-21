f302dd6270fbc8e64984248d3258f3f35ea746a5
NogaSapirHen <noga.sapirhen@post.runi.ac.il>
2023-12-21 14:02:43 +0200

    Add awesome code for Homework 1

--- AddTwo.java
+++ AddTwo.java
@@ -4,5 +4,11 @@
 public class AddTwo {
 	public static void main(String[] args) {
 		// Put your code here	
+		
+        // Declares integers
+	int a = Integer.parseInt (args[0]);
+	int b = Integer.parseInt (args[1]);
+		// prints the equlation and it's solution
+		System.out.println( a + " " + "+" + " " + b + " =" + " " + (a+b) );
 	}
 }
